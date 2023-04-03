d2a4e3def263310a986e501a1ebf42d35c8eff11
Mwangilwa Zimba <mwangilwa23@gmail.com>
2023-04-03 10:20:55 +0200

    Update list.h

--- 0x13-more_singly_linked_lists/list.h
+++ 0x13-more_singly_linked_lists/list.h
@@ -31,23 +31,3 @@ size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
 #endif
-=============================
-0-print_listint.c
-#include "lists.h"
-/**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
- *
- * Return: number of nodes
- */
-size_t print_listint(const listint_t *h)
-{
- size_t num = 0;
- while (h)
- {
- printf("%d\n", h->n);
- num++;
- h = h->next;
- }
- return (num);
-}
