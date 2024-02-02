d2a43bf18fb9b73c33aa4daf912ae0f4a0d42810
Mwangilwa Zimba <mwangilwa23@gmail.com>
2024-02-02 10:10:58 +0200

    commit

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_dlistint - print elements for list
+ * @h: head of list
+ * Return: nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	int count;
+
+	count = 0;
+
+	if (h == NULL)
+	{
+		return (count);
+	}
+
+	while (h->prev != NULL)
+	{
+		h = h->prev;
+	}
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
